40928fc2996833c15eada8184eb7d68fc2478e63
Xi Zhou <xxi.zhou@tum.de>
2023-05-05 16:14:02 +0200

    update ip adr

--- exercise01/models/index.js
+++ exercise01/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI ||
-    "mongodb://20.31.133.140:27017/booksData",{ useNewUrlParser: true });
+    "mongodb://localhost:27017/booksData",{ useNewUrlParser: true });
 
 module.exports.books = require("./books.js");
