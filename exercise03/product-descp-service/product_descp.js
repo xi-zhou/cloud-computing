84f30e3e933d744ede9d68455da169716c607230
Xi Zhou <xxi.zhou@tum.de>
2023-06-03 16:16:37 +0200

    solution 03

--- exercise03/product-descp-service/product_descp.js
+++ exercise03/product-descp-service/product_descp.js
@@ -7,6 +7,29 @@ module.exports = function (options) {
     this.add('role:product,cmd:getProductName', productName);
 
 
-    //To DO: add the pattern functions and describe the logic inside the function
+    //add the pattern functions and describe the logic inside the function
+    function productURL(msg, respond) {
+        if (msg.productId) {
+            var res = mockData.find(function (item) {
+                return item.product_id == msg.productId;
+            });
+            respond(null, { result: res.product_url });
+        }
+        else {
+            respond(null, { result: '' });
+        }
+    }
+    function productName(msg, respond) {
+        if (msg.productId) {
+            var res = mockData.find(function (item) {
+                return item.product_id == msg.productId;
+            });
+            respond(null, { result: res.product_name });
+        }
+        else {
+            respond(null, { result: '' });
+        }
+    }
+
 
 }
--- exercise03/product-price-service/product_price.js
+++ exercise03/product-price-service/product_price.js
@@ -1,9 +1,19 @@
 module.exports = function (options) {
     //Import the mock data json file
     const mockData = require('./MOCK_DATA.json');
-    //To DO: Add the patterns and their corresponding functions
+    //Add the patterns and their corresponding functions
+    this.add('role:product,cmd:getProductPrice', productPrice);
 
-
-    //To DO: add the pattern functions and describe the logic inside the function
-
+    //add the pattern functions and describe the logic inside the function
+    function productPrice(msg, respond) {
+        if (msg.productId) {
+            var res = mockData.find(function (item) {
+                return item.product_id == msg.productId;
+            });
+            respond(null, { result: res.product_price });
+        }
+        else {
+            respond(null, { result: '' });
+        }
+    }
 }
